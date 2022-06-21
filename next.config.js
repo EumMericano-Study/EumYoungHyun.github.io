465738a25a4f47f8dbaab83570a58f7eb7ed2dff
Eumericano <opwer032@naver.com>
2022-06-21 09:27:09 +0900

    Deploy Next.js to gh-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  exportPathMap: async function (defaultPathMap) {
+    const pathMap = { ...defaultPathMap };
 
-module.exports = nextConfig
+    for (const [path, config] of Object.entries(defaultPathMap)) {
+      if (path === "/") {
+        pathMap[path] = config;
+      } else {
+        pathMap[`${path}/index`] = config;
+      }
+    }
+
+    return pathMap;
+  },
+};
+
+module.exports = nextConfig;
